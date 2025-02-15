d8b5937c046af0955f58bc6f61147ba187de3bbb
RamboGj <jpramboguanabara@hotmail.com>
2025-02-15 14:13:11 -0300

    feat: responsive /unidade-digital

--- src/app/unidade-digital/page.tsx
+++ src/app/unidade-digital/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ArrowRightIcon } from '@/components/@icons/arrow-right'
 import { Button } from '@/components/@solumedi-ui/atoms/Button/Button'
 import { Heading } from '@/components/@solumedi-ui/atoms/Heading/Heading'
@@ -16,6 +18,7 @@ import {
 import { HeaderUnity } from '@/components/molecules/HeaderUnity/HeaderUnity'
 import { Footer } from '@/components/molecules/Footer'
 import { CheckIcon } from '@/components/@icons/check'
+import { useDeviceType } from '@/hooks/useDeviceType'
 
 export default function DigitalUnityPage() {
   const plans: PricingProps[] = [
@@ -122,6 +125,8 @@ export default function DigitalUnityPage() {
     'Cirurgias e Atendimento Personalizado',
   ]
 
+  const { isMobile } = useDeviceType()
+
   return (
     <div className="w-full overflow-x-hidden">
       <section
@@ -149,9 +154,12 @@ export default function DigitalUnityPage() {
         </main>
       </section>
 
-      <section className="block xl:hidden px-12 mt-8">
+      <section className="block xl:hidden px-6 md:px-12 mt-8">
         <div className="w-full">
-          <Heading className="w-[60%]" variant="h2">
+          <Heading
+            className="w-full text-left md:w-[60%]"
+            variant={isMobile ? 'h3' : 'h2'}
+          >
             Consultas médicas com valores reduzidos
           </Heading>
 
@@ -161,34 +169,38 @@ export default function DigitalUnityPage() {
           </Paragraph>
         </div>
 
-        <div className="flex mt-10 gap-x-4">
+        <div className="flex-col md:flex-row flex mt-10 gap-4">
           <Button label="Agendar pelo WhatsApp" icon={<ArrowRightIcon />} />
           <Button label="Ligar para agendar" variant="light-blue" />
         </div>
       </section>
 
       <section
-        className="w-full flex flex-col gap-y-20 items-center my-20"
+        className="w-full flex flex-col gap-y-20 items-center my-20 px-6 md:px-0"
         id="cards"
       >
         <Heading
           className="max-w-[554px] xl:max-w-[892px] w-full text-center text-blue500"
-          variant="h2"
+          variant={isMobile ? 'h3' : 'h2'}
         >
           Economize com a SoluMedi para se consultar ou fazer exames
         </Heading>
 
-        <div className="grid grid-cols-2 lg:grid-cols-4 grid-rows-2 mx-[56px] items-center gap-6">
-          <div className="bg-blue100 rounded-[48px] h-[268px] p-8 2xl:p-[56px] order-1 grid row-span-1 col-span-1">
+        <div className="grid grid-cols-2 lg:grid-cols-4 grid-rows-2 md:mx-[56px] items-center gap-6">
+          <div className="bg-blue100 rounded-[24px] md:rounded-[48px] h-[240px] md:h-[268px] p-6 md:p-8 2xl:p-[56px] order-1 grid row-span-1 col-span-1">
             <Heading
-              className="w-full 2xl:w-[232px] block mt-auto"
+              className="w-full 2xl:w-[232px] hidden md:block mt-auto"
               variant="h4"
             >
               Cadastro fácil, rápido e totalmente gratuito.
             </Heading>
+
+            <Paragraph size="sm" className="w-full block md:hidden mt-auto">
+              Cadastro fácil, rápido e totalmente gratuito.
+            </Paragraph>
           </div>
 
-          <div className="bg-neutral100 border border-blue200 h-full rounded-[48px] order-2 p-8 2xl:p-[56px] grid row-span-2 col-span-1">
+          <div className="bg-neutral100 border border-blue200 h-full rounded-[24px] md:rounded-[48px] order-3 md:order-2 p-6 md:p-8 2xl:p-[56px] grid row-span-2 col-span-2 md:col-span-1">
             <div className="mt-auto flex flex-col">
               <Heading variant="h4">A partir de</Heading>
 
@@ -206,18 +218,30 @@ export default function DigitalUnityPage() {
             </div>
           </div>
 
-          <div className="bg-blue100 relative overflow-hidden rounded-[48px] h-[500px] lg:h-full p-8 2xl:p-[56px] order-4 lg:order-3 grid row-span-2 col-span-2">
+          <div className="bg-blue100 relative overflow-hidden rounded-[24px] md:rounded-[48px] h-[500px] lg:h-full p-6 md:p-8 2xl:p-[56px] order-4 lg:order-3 grid row-span-2 col-span-2">
             <Image
               alt="Cartão Solumedi"
               src={solumediCardImg}
-              className="absolute top-0 inset-x-0 w-full h-[300px] lg::h-[255px] 2xl:h-[400px] object-cover"
+              className="absolute top-0 inset-x-0 w-full h-[300px] lg:h-[255px] 2xl:h-[400px] object-cover"
             />
 
             <div className="mt-auto flex items-end justify-between">
               <div className="max-w-[400px] w-full">
-                <Heading variant="h2">Cartão SoluMedi</Heading>
+                <Heading className="md:block hidden" variant="h2">
+                  Cartão SoluMedi
+                </Heading>
+                <Heading className="md:hidden" variant="h3">
+                  Cartão SoluMedi
+                </Heading>
 
-                <Paragraph className="text-blue500 block mt-6" size="md">
+                <Paragraph
+                  className="text-blue500 hidden md:block mt-6"
+                  size="md"
+                >
+                  Gratuito, rapido e fácil de usar. Sem cobrança de mensalidade
+                  e anuidade. Você só paga quando precisa usar.
+                </Paragraph>
+                <Paragraph className="text-blue500 md:hidden mt-6" size="sm">
                   Gratuito, rapido e fácil de usar. Sem cobrança de mensalidade
                   e anuidade. Você só paga quando precisa usar.
                 </Paragraph>
@@ -225,18 +249,22 @@ export default function DigitalUnityPage() {
             </div>
           </div>
 
-          <div className="bg-blue400 rounded-[48px] h-[268px] p-8 2xl:p-[56px] order-3 lg:order-4 grid row-span-1 col-span-1">
+          <div className="bg-blue400 rounded-[24px] md:rounded-[48px] h-[240px] md:h-[268px] p-6 md:p-8 2xl:p-[56px] order-2 md:order-3 lg:order-4 grid row-span-1 col-span-1">
             <Heading
-              className="w-full 2xl:w-[232px] block mt-auto text-neutral100"
+              className="w-full 2xl:w-[232px] hidden md:block mt-auto text-white"
               variant="h4"
             >
               Pague só quando usar, sem mensalidades.
             </Heading>
+
+            <Paragraph size="sm" className="flex md:hidden mt-auto text-white">
+              Pague só quando usar, sem taxas mensais.
+            </Paragraph>
           </div>
         </div>
       </section>
 
-      <section className="py-20 mx-12 2xl:mx-[210px] flex flex-col-reverse gap-y-10 lg:flex-row items-center justify-between">
+      <section className="py-20 mx-6 md:mx-12 2xl:mx-[210px] flex flex-col-reverse gap-y-10 lg:flex-row items-center justify-between">
         <Image
           objectFit="cover"
           priority
@@ -244,11 +272,14 @@ export default function DigitalUnityPage() {
           height={640}
           src="/consultaAgendada.png"
           alt="Doctor looking at his cellphone."
-          className="w-[578px] h-[502px] 2xl:w-[738px] 2xl:h-[640px]"
+          className="w-[320px] h-[277px] md:w-[578px] md:h-[502px] 2xl:w-[738px] 2xl:h-[640px]"
         />
 
         <div className="max-w-[436px] xl:max-w-[479px] 2xl:max-w-[584px] w-full flex flex-col gap-y-10">
-          <Heading variant="h2">
+          <Heading className="hidden md:block" variant="h2">
+            Pague menos para ter acesso a saúde de qualidade
+          </Heading>
+          <Heading className="md:hidden" variant="h3">
             Pague menos para ter acesso a saúde de qualidade
           </Heading>
 
@@ -256,7 +287,7 @@ export default function DigitalUnityPage() {
             {features.map((feature) => {
               return (
                 <li key={feature}>
-                  <div className="flex items-center gap-x-4">
+                  <div className="flex items-start md:items-center gap-x-4">
                     <CheckIcon className="text-blue400" />
                     <Paragraph className="text-blue500">{feature}</Paragraph>
                   </div>
@@ -267,25 +298,34 @@ export default function DigitalUnityPage() {
         </div>
       </section>
 
-      <section id="plans" className="flex flex-col lg:items-center py-[120px]">
+      <section
+        id="plans"
+        className="flex flex-col lg:items-center py-16 md:py-[120px]"
+      >
         <Heading
-          className="text-blue500 max-w-[586px] w-full text-center mx-auto px-12 2xl:mx-[210px]"
+          className="text-blue500 max-w-[586px] hidden md:block w-full text-center mx-auto px-12 2xl:mx-[210px]"
           variant="h2"
         >
           Compare valores da SoluMedi com o mercado
         </Heading>
+        <Heading
+          className="text-blue500 w-full text-left px-6 md:hidden"
+          variant="h3"
+        >
+          Compare valores da SoluMedi com o mercado
+        </Heading>
 
-        <ul className="w-full flex lg:grid lg:grid-cols-3 gap-x-6 mt-20 snap-x snap-mandatory overflow-x-scroll scrollbar-none px-12 2xl:mx-[210px]">
+        <ul className="w-full flex lg:grid lg:grid-cols-3 gap-x-6 mt-8 md:mt-20 snap-x snap-mandatory overflow-x-scroll scrollbar-none px-6 md:px-12 2xl:mx-[210px]">
           {plans.map((plan, index) => {
             return (
               <li className="snap-center" key={index}>
-                <PricingCard className="min-w-[296px] lg:min-w-fit" {...plan} />
+                <PricingCard className="min-w-[296px]" {...plan} />
               </li>
             )
           })}
         </ul>
 
-        <Caption className="max-w-[520px] w-full text-center mt-10">
+        <Caption className="md:max-w-[520px] px-6 md:px-0 w-full text-left md:text-center mt-6 md:mt-10">
           Valores meramente ilustrativos com base nos preços praticados pelo
           mercado. A SoluMedi não garante valores de consultas e exames médicos.
         </Caption>
@@ -296,7 +336,7 @@ export default function DigitalUnityPage() {
         id="steps"
       >
         <Heading
-          className="text-[4rem] leading-[100%] text-center mx-12 lg:mx-0"
+          className="text-[2rem] leading-[125%] md:text-[4rem] md:leading-[100%] text-center mx-12 lg:mx-0"
           variant="h2"
         >
           É rápido, fácil e você só paga se usar
--- src/components/molecules/HeaderUnity/HeaderUnity.tsx
+++ src/components/molecules/HeaderUnity/HeaderUnity.tsx
@@ -2,19 +2,25 @@ import NextLink from 'next/link'
 
 import { Logo } from '@/components/@solumedi-ui/atoms/Logo/Logo'
 import { Paragraph } from '@/components/@solumedi-ui/atoms/Paragraph/Paragraph'
+import { Caption } from '@/components/@solumedi-ui/atoms/Caption/Caption'
 
 export function HeaderUnity() {
   return (
-    <header className="h-[104px] w-full flex items-center justify-between px-[56px]">
+    <header className="h-[90px] md:h-[104px] w-full flex items-center justify-between px-6 md:px-12 lg:px-[56px] bg-white lg:bg-transparent">
       <NextLink href="/">
         <Logo />
       </NextLink>
 
-      <div className="flex flex-col items-end">
-        <Paragraph size="md" className="text-blue500">
+      <div className="hidden md:flex flex-col items-end">
+        <Paragraph size="sm" className="text-blue500">
           Unidade franqueada
         </Paragraph>
-        <Paragraph size="md">Curitiba - Augusta</Paragraph>
+        <Paragraph size="sm">Curitiba - Augusta</Paragraph>
+      </div>
+
+      <div className="flex md:hidden flex-col items-end">
+        <Caption className="text-blue500">Unidade franqueada</Caption>
+        <Caption>Curitiba - Augusta</Caption>
       </div>
     </header>
   )
